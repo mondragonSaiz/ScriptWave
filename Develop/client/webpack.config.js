6e4cb6618c474f9813ba0546645d26ad88edffc6
David Mondragon <dragon18saiz@gmail.com>
2023-08-04 19:34:36 -0600

    Added database functions getdb, putdb, routerRegister on sw

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -58,7 +58,7 @@ module.exports = () => {
         },
         {
           test: /\.m?js$/,
-          exlude: /node_modules/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
